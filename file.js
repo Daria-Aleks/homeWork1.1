e75ca5c83fbf51b3eae60942e606600986132534
Daria <daria.aleks1@yandex.ru>
2023-09-29 15:13:29 +0300

    hw4

---
+++ file.js
@@ -0,0 +1,17 @@
+let i='Привет';
+while (i<3) {
+    console.log (i);
+    i++;
+}
+//
+let a = 1;
+while (a<=5) {
+    console.log (a);
+    a++;
+}
+//
+let b = 7;
+while (b<=22) {
+    console.log (b);
+    b++;    
+}
