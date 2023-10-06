b551928b81d7768efbf440e71cf0dd958adf2699
Daria <daria.aleks1@yandex.ru>
2023-10-06 22:35:54 +0300

    fixed

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ function game() {
                     alert('Такого месяца у нас нет, солнце')
                 }
 }
+
+function words() {
+    const words = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+const shuffledWords = shuffleArray(words);
+alert(shuffledWords.join(', '));
+const firstWord = prompt('Чему равнялся первый элемент массива?');
+const lastWord = prompt('Чему равнялся последний элемент массива?');
+if (firstWord === words[0] && lastWord === words[words.length - 1]) {
+  alert('Поздравляю, вы угадали оба элемента!');
+} else if (firstWord === words[0] || lastWord === words[words.length - 1]) {
+  alert('Вы были близки к победе!');
+} else {
+  alert('Вы ответили неверно.');
+}
+function shuffleArray(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+}
